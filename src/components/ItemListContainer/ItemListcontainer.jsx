a69a84fc07d688816517ad9c1aedc1f13a088cf3
naghuu95 <nahuel_mercado@live.com>
2023-05-03 02:56:47 -0300

    usamos los datos de firebase para renderizar nuestros productos a nuestro sitio web, modficamos itemlistContainer y reemplazamos nuestro getProductos por Firestore

--- src/components/ItemListContainer/ItemListcontainer.jsx
+++ src/components/ItemListContainer/ItemListcontainer.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState, } from "react"
-import { getProductos,getProductosByCategoria } from "../../asyncMock/asyncMock"
+//import { getProductos,getProductosByCategoria } from "../../asyncMock/asyncMock"
+//Importamos la funcion getFirestore para consumir nuestra base de datos 
+import { getFirestore } from "../../firebase/config"
 import { ItemList } from "../ItemList/ItemList"
 import './ItemListContainer.css'
 import { useParams } from "react-router-dom"
+import {ImSpinner3} from 'react-icons/im'
 
 
 
@@ -10,11 +13,77 @@ import { useParams } from "react-router-dom"
 const ItemListContainer =({greeting})=>{
     
     const [productos, setProductos]= useState([])
+
+    const [loading,setLoading]=useState(false)
     
     const{ categoriaId }= useParams()
 
        useEffect(()=>{
-        const asyncFunc= categoriaId ? getProductosByCategoria : getProductos
+
+        setLoading(true)
+
+        const db = getFirestore();
+
+        const productos= db.collection('productos')
+
+        if(categoriaId){
+            const filtrado = productos.where("categoria","==",categoriaId)
+            filtrado.get()
+                .then((res)=>{
+                    const newItem =res.docs.map ((doc)=>{
+                        return{id: doc.id, ...doc.data()}
+                    })
+                    setProductos(newItem)
+                })
+                .catch ((err)=>console.log(err))
+                .finally (()=>{
+                    setLoading(false)
+                })
+        }
+
+        else{
+            productos.get()
+            .then((res)=>{
+               const newItem =res.docs.map((doc)=>{
+                   return {id: doc.id, ...doc.data()}
+               })
+               console.table(newItem)
+               setProductos(newItem)
+            })
+            .catch((err)=>console.log(err))
+            .finally(()=>{
+               setLoading(false)
+            })
+      
+        }
+
+        
+       },[categoriaId,setLoading])
+
+
+
+   return (
+
+    <>
+    {/* Nuestro componente arranca con el loading en "true" y cuando resulve, imprime en pantalla todo nuestro componente ItemList (donde mapeamos cada uno de los productos) */}
+      {
+        loading
+        ?<div className='spinner'><ImSpinner3/></div>
+        : 
+        <ItemList productos={productos}/>
+      }
+    </>
+
+    
+   )
+}
+
+export default ItemListContainer
+
+
+
+ 
+        /*const asyncFunc= categoriaId ? getProductosByCategoria : getProductos
 
         asyncFunc(categoriaId)
             .then(response=>{
@@ -22,18 +91,4 @@ const ItemListContainer =({greeting})=>{
             })
             .catch(error=>{
                 console.error(error)
-            })
-
-       },[categoriaId])
-
-
-
-   return (
-    <div>
-        <h1> {greeting}</h1>
-        <ItemList productos={productos}/>
-    </div>
-   )
-}
-
-export default ItemListContainer
+            })*/
